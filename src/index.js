e8bd89a36ff433cf2c7d08b15bc8368f2ddfadf2
yoga1234 <septianayoga30@gmail.com>
2026-03-12 17:07:24 +0700

    adding click event for the menu buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 
+// menu related section
+let currentPage = "None";
+
 // generating content
 const content = document.getElementById("main-content");
 
@@ -9,3 +12,37 @@ paragraph.textContent = "Testing generating content using webpack";
 content.appendChild(paragraph);
 
 // create event for menu click and change the content base on menu
+const menuButtons = document.getElementsByClassName("menu-link");
+
+// function for menu button
+const menuClickEvent = (e) => {
+  // menu changing using object mapping
+  const pages = {
+    None: renderHome,
+    Home: renderHome,
+    Menu: renderMenu,
+    About: renderAbout,
+  };
+
+  currentPage = e.target.innerText;
+  pages[currentPage]?.();
+};
+
+// adding click event listener for menu button
+for (var i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener("click", menuClickEvent);
+}
+
+// functions for rendering content
+const renderHome = () => {
+  content.innerHTML = "<h1>Ini adalah renderHome</h1>";
+};
+const renderMenu = () => {
+  content.innerHTML = "<h1>Ini adalah renderMenu</h1>";
+};
+const renderAbout = () => {
+  content.innerHTML = "<h1>Ini adalah renderAbout</h1>";
+};
+
+// rendering home for the first time
+renderHome();
