835c15f74e4d637b830050e670f716ff81c58ff9
yoga1234 <septianayoga30@gmail.com>
2026-03-12 15:03:20 +0700

    adding navigation bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
 
-console.log("Yogasmara is here!");
+// generating content
+const content = document.getElementById("main-content");
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "Testing generating content using webpack";
+
+content.appendChild(paragraph);
+
+// create event for menu click and change the content base on menu
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      favicon: "./src/img/favicon.ico",
     }),
   ],
   module: {
